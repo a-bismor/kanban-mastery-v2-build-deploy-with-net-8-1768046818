f1ae4c65f77816db5cc0bda0fc33d889ff601e72
Adam Bismor <dev@bismor.pl>
2026-01-25 20:00:11 +0100

    feat(models): add Board, Column, Card, ApplicationUser and BoardMember

---
+++ KanbanApi/Models/BoardMember.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class BoardMember
+{
+    public int BoardId { get; set; }
+    public Board Board { get; set; } = null!;
+    public string UserId { get; set; } = "";
+    public ApplicationUser User { get; set; } = null!;
+    public string Role { get; set; } = "Member";
+}
