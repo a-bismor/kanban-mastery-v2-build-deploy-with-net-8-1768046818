c790d643e63b9548fee7f8cbf458e865369463c4
Adam Bismor <dev@bismor.pl>
2026-01-26 22:06:47 +0100

    feat: add ApplicationDbContext, Identity integration, and initial migration

--- KanbanApi/Models/ApplicationUser.cs
+++ KanbanApi/Models/ApplicationUser.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
+
 namespace KanbanApi.Models;
 
 public class ApplicationUser : IdentityUser
 {
-
+    public ICollection<BoardMember> BoardMemberships { get; set; } = new List<BoardMember>();
 }
